f4ec9edab5ab7d31607343f3cd51abc5a50d1973
NamelessOne <nlessone@gmail.com>
2017-03-19 08:56:51 +0500

    external services integration tests

--- FantasyRadio/src/test/java/externalServicesIntegrationTests/ScheduleGetterTest.java
+++ FantasyRadio/src/test/java/externalServicesIntegrationTests/ScheduleGetterTest.java
@@ -1,8 +1,32 @@
 package externalServicesIntegrationTests;
 
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import ru.sigil.fantasyradio.schedule.ScheduleEntity;
+import ru.sigil.fantasyradio.schedule.ScheduleParser;
+
 /**
- * Created by namelessone on 19.03.17.
+ * Created by
+ * namelessone on 19.03.17.
  */
 
 public class ScheduleGetterTest {
+    @Test
+    public void Get_Schedule_Positive_Test() throws Exception
+    {
+        ScheduleParser scheduleParser = new ScheduleParser();
+        List<ScheduleEntity> result = scheduleParser.ParseSchedule();
+        Assert.assertNotNull(result);
+        Assert.assertFalse(result.isEmpty());
+        for (ScheduleEntity entity : result) {
+            Assert.assertFalse(entity.getText().isEmpty());
+            Assert.assertFalse(entity.getTitle().isEmpty());
+            Assert.assertNotNull(entity.getEndDate());
+            Assert.assertNotNull(entity.getStartDate());
+        }
+    }
 }
