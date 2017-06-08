2782fdb6bf48f7c9870459b51f3138691696d81f
NamelessOne <nlessone@gmail.com>
2017-06-08 20:25:45 +0500

    Change menu item background. Fix incoming call mute bug.

--- FantasyRadio/src/main/java/ru/sigil/fantasyradio/MyApp.java
+++ FantasyRadio/src/main/java/ru/sigil/fantasyradio/MyApp.java
@@ -2,10 +2,6 @@ package ru.sigil.fantasyradio;
 
 import android.app.Application;
 import android.content.Context;
-import android.telephony.PhoneStateListener;
-import android.telephony.TelephonyManager;
-
-import com.un4seen.bass.BASS;
 
 import org.acra.ACRA;
 import org.acra.ErrorReporter;
@@ -18,33 +14,11 @@ import ru.sigil.log.LogManager;
 @ReportsCrashes(formKey = "", formUri = "http://fantasyradionotifications-sigil.rhcloud.com/crash", logcatArguments = {
         "-t", "50"})
 public class MyApp extends Application {
-    private float vol = (float) 0.5;
 
     @Override
     public void onCreate() {
         super.onCreate();
         Bootstrap.INSTANCE.setup(this);
-        try {
-            PhoneStateListener phoneStateListener = new PhoneStateListener() {
-                @Override
-                public void onCallStateChanged(int state, String incomingNumber) {
-                    if (state == TelephonyManager.CALL_STATE_RINGING) {
-                        vol = BASS.BASS_GetVolume();
-                        BASS.BASS_SetVolume(0);
-                    } else if (state == TelephonyManager.CALL_STATE_IDLE) {
-                        BASS.BASS_SetVolume(vol);
-                    }
-                    super.onCallStateChanged(state, incomingNumber);
-                }
-            };
-            TelephonyManager mgr = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-            if (mgr != null) {
-                mgr.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
-            }
-        }catch (Exception e) {
-            //TODO лучше бы знать про это событие
-            e.printStackTrace();
-        }
         LogManager.addLogger(new DefaultLogger());
     }
 
---
+++ FantasyRadio/src/main/java/ru/sigil/fantasyradio/PhoneStateReceiver.java
@@ -0,0 +1,38 @@
+package ru.sigil.fantasyradio;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.un4seen.bass.BASS;
+
+/**
+ * Created by namelessone
+ * on 08.06.17.
+ */
+
+public class PhoneStateReceiver extends BroadcastReceiver {
+    private float vol = (float) 0.5;
+
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+        try {
+            String str = intent.getAction();
+            if ("android.intent.action.PHONE_STATE".equals(str))
+                inComing(context, intent);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private void inComing(Context context, Intent intent) {
+        String callState = intent.getStringExtra("state");
+        if ("RINGING".equals(callState) || "OFFHOOK".equals(callState)) {
+            vol = BASS.BASS_GetVolume();
+            BASS.BASS_SetVolume(0);
+        } else if ("IDLE".equals(callState)) {
+            BASS.BASS_SetVolume(vol);
+        }
+    }
+}
